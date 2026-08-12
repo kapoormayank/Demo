5f7cab7dbbeef4234ffcb58471bf4dd767ff7f18
kapoormayank9696 <kapoormayank645@gmail.com>
2026-08-12 20:59:46 +0530

    Data Structure And Algorithms

---
+++ DSA/Loops/AddNumber.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class AddNumber {
+    static class Solution {
+        public int addDigits(int num) {
+            if(num == 0) { return 0; }
+
+            return 1 + (num-1) % 9;
+        }
+    }
+
+    // Main function
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        System.out.println(solution.addDigits(num)); // Output: 2
+
+        sc.close();
+    }
+}
+
