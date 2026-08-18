e25ca80f8fb20bfd13ea1b36324c57d7c8d516c5
kapoormayank9696 <kapoormayank645@gmail.com>
2026-08-18 21:41:52 +0530

    Data Structure And Algorithms

---
+++ DSA/Graph/UserInput.java
@@ -0,0 +1,12 @@
+// Graph User Input Algorithm Implementation In Java
+
+import java.util.Scanner;
+
+public class UserInput {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of vertices: ");
+        int vertices = sc.nextInt();
+    }
+}
