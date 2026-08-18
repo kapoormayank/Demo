3571e0abf0a740c64bf8805f807c25eb4af46971
kapoormayank9696 <kapoormayank645@gmail.com>
2026-08-18 21:43:48 +0530

    Data Structure And Algorithms

--- DSA/Graph/UserInput.java
+++ DSA/Graph/UserInput.java
@@ -4,9 +4,11 @@ import java.util.Scanner;
 
 public class UserInput {
 
+    @SuppressWarnings("unused")
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the number of vertices: ");
-        int vertices = sc.nextInt();
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter the number of vertices: ");
+            int vertices = sc.nextInt();
+        }
     }
 }
