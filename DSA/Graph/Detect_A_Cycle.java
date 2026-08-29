5b085c60f3ac3802774b6847791feecef11df44b
kapoormayank9696 <kapoormayank645@gmail.com>
2026-08-29 13:50:40 +0530

    Data Structure And Algorithms

--- DSA/Graph/Detect_A_Cycle.java
+++ DSA/Graph/Detect_A_Cycle.java
@@ -10,8 +10,28 @@ public class Detect_A_Cycle {
         // Data Members And Public Specifiers
         public int V; // Number of vertices in the graph
         public ArrayList<Integer>[] adj; // Adjacency list representation of the graph
+
+        // Parameterized Constructor
+        @SuppressWarnings("unchecked")
+        public Graph(int V) {
+            this.V = V;
+            adj = new ArrayList[V];
+            for (int i = 0; i < V; i++) {
+                adj[i] = new ArrayList<>();
+            }
+        }
+
+        // Add an edge to the graph
+        public void addEdge(int u, int v) {
+            adj[u].add(v);
+        }
+
+        // Function to detect a cycle in the graph using DFS
+        // Print Graph
+        
     }
 
+    // Main function
     public static void main(String[] args) {
 
     }
