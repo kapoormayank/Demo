4b696d72f056947ce0ba61a4fc5e0583fbc042b1
kapoormayank9696 <kapoormayank645@gmail.com>
2026-08-29 12:49:48 +0530

    Data Structure And Algorithms

---
+++ DSA/Graph/Detect_A_Cycle.java
@@ -0,0 +1,18 @@
+// Detect a cycle in a directed graph using DFS Algorithm Implement in a Java
+
+import java.util.ArrayList;
+
+public class Detect_A_Cycle {
+
+    // Graph Class
+    @SuppressWarnings("unused")
+    public static class Graph {
+        // Data Members And Public Specifiers
+        public int V; // Number of vertices in the graph
+        public ArrayList<Integer>[] adj; // Adjacency list representation of the graph
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
