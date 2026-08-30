c3f79aa465dcd9ff06f0572dbd3d3f5247b778c5
kapoormayank9696 <kapoormayank645@gmail.com>
2026-08-30 13:31:47 +0530

    Data Structure And Algorithms

--- DSA/Graph/Detect_A_Cycle.java
+++ DSA/Graph/Detect_A_Cycle.java
@@ -24,15 +24,38 @@ public class Detect_A_Cycle {
         // Add an edge to the graph
         public void addEdge(int u, int v) {
             adj[u].add(v);
+            adj[v].add(u); // For undirected graph, add this line. For directed graph, comment this line.
         }
 
         // Function to detect a cycle in the graph using DFS
+
         // Print Graph
+        void printAdjList() {
+            for (int i = 0; i < V; i++) {
+                System.out.print("Vertex " + i + ": ");
+                for (int j : adj[i]) {
+                    System.out.print(j + " ");
+                }
+                System.out.println();
+            }
+        }
         
     }
 
     // Main function
     public static void main(String[] args) {
+         
+        // Create a graph with 5 vertices
+        Graph graph = new Graph(5);
+
+        // Add edges to the graph
+        graph.addEdge(0, 1);
+        graph.addEdge(1, 2);
+        graph.addEdge(1, 3);
+        graph.addEdge(2, 4); 
+
+        System.out.println("Graph representation (Adjacency List):");
+        graph.printAdjList();
 
     }
 }
