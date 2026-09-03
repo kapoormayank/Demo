d98f424d1edf4e2cdd1eeba467ea41be295b5e7f
kapoormayank9696 <kapoormayank645@gmail.com>
2026-09-03 14:56:24 +0530

    Data Structure And Algorithms

---
+++ DSA/Graph/Number_Of_Island.java
@@ -0,0 +1,58 @@
+// Count the number of islands in a 2D grid using BFS Algorithm Implement in a Java
+
+import java.util.ArrayList;
+
+public class Number_Of_Island {
+
+    // Graph Class
+    public static class Graph {
+        
+        // Data Members And Public Access Specifier
+        public int V; // Number of vertices in the graph
+        public ArrayList<Integer>[] adj; // Adjacency list representation of the graph
+
+        @SuppressWarnings("unchecked")
+        // Parameterized Constructor
+        public Graph(int V) {
+            this.V = V;
+            adj = new ArrayList[V];
+
+            for(int i=0; i<V; i++) {
+                adj[i] = new ArrayList<>();
+            }
+        }
+
+        // Add an edge to the graph
+        void addEdge(int u,int v) {
+            adj[u].add(v);
+            adj[v].add(u);
+        }
+
+        // Print Adjanency Graph
+        public void printGraph() {
+
+            for(int i=0; i<V; i++) {
+                System.out.print("Vertex: "+i+": ");
+                for(int neighbor : adj[i]) {
+                    System.out.print(neighbor+" ");
+                }
+                System.err.print("\n");
+            }
+            
+        }
+    }
+
+    // Main function
+    public static void main(String[] args) {
+        // Create a graph with 5 vertices
+        Graph graph = new Graph(5);
+
+        graph.addEdge(0,1);
+        graph.addEdge(1, 2);
+        graph.addEdge(1, 3);
+        graph.addEdge(2, 4);
+
+        System.out.println("Graph representation (Adjacency List):");
+        graph.printGraph();
+    }
+}
