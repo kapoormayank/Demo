4803148541372bb9d8a0a5ee34f53b567a0c0ee9
kapoormayank9696 <kapoormayank645@gmail.com>
2026-09-04 17:47:34 +0530

    Data Structure And Algorithms

--- DSA/Graph/Number_Of_Island.java
+++ DSA/Graph/Number_Of_Island.java
@@ -1,32 +1,9 @@
 // Count the number of islands in a 2D grid using BFS Algorithm Implement in a Java
 
-import java.util.ArrayList;
-
 public class Number_Of_Island {
 
     // Graph Class
     public static class Graph {
-        
-        // Data Members And Public Access Specifier
-        public int V; // Number of vertices in the graph
-        public ArrayList<Integer>[] adj; // Adjacency list representation of the graph
-
-        @SuppressWarnings("unchecked")
-        // Parameterized Constructor
-        public Graph(int V) {
-            this.V = V;
-            adj = new ArrayList[V];
-
-            for(int i=0; i<V; i++) {
-                adj[i] = new ArrayList<>();
-            }
-        }
-
-        // Add an edge to the graph
-        void addEdge(int u,int v) {
-            adj[u].add(v);
-            adj[v].add(u);
-        }
 
         // Island Count Function
         public int countIslands(int[][] grid) {
@@ -36,7 +13,7 @@ public class Number_Of_Island {
             for(int i=0; i<grid.length; i++) {
                 for(int j=0; j<grid[0].length; j++) {
                     if(grid[i][j] == 1 && !visited[i][j]) {
-                        bfs(grid, visited, i, j);
+                        dfs(grid, visited, i, j);
                         count++;
                     }
                 }
@@ -45,32 +22,34 @@ public class Number_Of_Island {
             return count;
         }
 
-        // Print Adjanency Graph
-        public void printGraph() {
-
-            for(int i=0; i<V; i++) {
-                System.out.print("Vertex: "+i+": ");
-                for(int neighbor : adj[i]) {
-                    System.out.print(neighbor+" ");
-                }
-                System.err.print("\n");
+        // Depth-First Search
+        private void dfs(int[][] grid, boolean[][] visited, int i, int j) {
+            if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == 0 || visited[i][j]) {
+                return;
             }
-            
+
+            visited[i][j] = true;
+
+            // Recursively visit all adjacent cells
+            dfs(grid, visited, i+1, j);
+            dfs(grid, visited, i-1, j);
+            dfs(grid, visited, i, j+1);
+            dfs(grid, visited, i, j-1);
         }
     }
 
     // Main function
     public static void main(String[] args) {
+        int[][] grid = {
+            {1, 1, 0, 0, 0},
+            {1, 1, 0, 0, 0},
+            {0, 0, 1, 0, 0},
+            {0, 0, 0, 1, 1}
+        };
+
+        Graph graph = new Graph();
         
-        // Create a graph with 5 vertices
-        Graph graph = new Graph(5);
-
-        graph.addEdge(0,1);
-        graph.addEdge(1, 2);
-        graph.addEdge(1, 3);
-        graph.addEdge(2, 4);
-
-        System.out.println("Graph representation (Adjacency List):");
-        graph.printGraph();
-    }
+        int numberOfIslands = graph.countIslands(grid);
+        System.out.println("Number of islands: " + numberOfIslands);
+    }       
 }
